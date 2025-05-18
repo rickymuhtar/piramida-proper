b57f0c06275e65479fcd97ff89616275a4712778
rickymuhtar <98572360+rickymuhtar@users.noreply.github.com>
2025-05-18 08:32:26 +0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PiramidaPROPER from './components/PiramidaPROPER';
+import './index.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Piramida PROPER</h1>
+      <PiramidaPROPER />
+    </div>
+  );
+}
+
+export default App;
